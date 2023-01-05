16c92484ee9df0eaa106cf3e74c070c5eae9021e
levblanc <levblanc@gmail.com>
2023-01-05 11:36:59 +0800

    Add `launches` page

---
+++ client/src/pages/launches.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { gql } from '@apollo/client';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
+interface LaunchesProps {}
+
+const Launches: React.FC<LaunchesProps> = () => {
+  return <div />;
+};
+
+export default Launches;
