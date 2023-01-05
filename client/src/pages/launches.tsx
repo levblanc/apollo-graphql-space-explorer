efef5ad47e9565db94546926228411d30c8abbd3
levblanc <levblanc@gmail.com>
2023-01-05 11:53:46 +0800

    Add launch list display & pagination

--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { gql } from '@apollo/client';
+import React, { Fragment, useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
 import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+import { LaunchTile, Header, Button, Loading } from '../components';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -34,7 +35,45 @@ export const GET_LAUNCHES = gql`
 interface LaunchesProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const { data, loading, error, fetchMore } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const handleLoadMore = async () => {
+    setIsLoadingMore(true);
+
+    await fetchMore({
+      variables: {
+        after: data?.launches.cursor,
+      },
+    });
+
+    setIsLoadingMore(false);
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => {
+          return <LaunchTile key={launch.id} launch={launch} />;
+        })}
+      {data.launches &&
+        data.launches.hasMore &&
+        (isLoadingMore ? (
+          <Loading />
+        ) : (
+          <Button onClick={handleLoadMore}>Load More</Button>
+        ))}
+    </Fragment>
+  );
 };
 
 export default Launches;
