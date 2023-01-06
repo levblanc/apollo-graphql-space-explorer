355ecc1edbec3b9f62ca5c9d8bec6ed7d1a10d15
levblanc <levblanc@gmail.com>
2023-01-06 18:29:37 +0800

    Update `cartItems` query & UI display logic

--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import React, { Fragment } from 'react';
+import { Header, Loading } from '../components';
+import { BookTrips, CartItem } from '../containers';
+import { GetCartItems } from './__generated__/GetCartItems';
 
-interface CartProps {
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
 
-}
+interface CartProps {}
 
 const Cart: React.FC<CartProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<GetCartItems>(GET_CART_ITEMS);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return (
+    <Fragment>
+      <Header>My Cart</Header>
+      {data?.cartItems.length === 0 ? (
+        <p data-testid='empty-message'>No items in your cart</p>
+      ) : (
+        <Fragment>
+          {data?.cartItems.map((launchId: any) => {
+            return <CartItem key={launchId} launchId={launchId} />;
+          })}
+          <BookTrips cartItems={data?.cartItems || []} />
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 export default Cart;
